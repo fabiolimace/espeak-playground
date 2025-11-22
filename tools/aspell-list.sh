d497616d736446279d91c9c6cfa182b649c62120
Fabio Lima <fabiolimace@gmail.com>
2025-11-22 19:21:14 -0300

    Update spell-list.sh

--- tools/aspell-list.sh
+++ tools/aspell-list.sh
@@ -3,17 +3,28 @@
 # 
 # List all misspelled words in a text file using Aspell.
 # 
+# Usage
+#
+#     aspell-list file.txt
+#     aspell-list file.txt pt_BR pt_PT
+#     aspell-list file.txt pt_BR pt_PT en_US
+#
+# Install dicts:
+#
+#     apt install aspell-pt-br aspell-pt-pt aspell-en aspell-uk aspell-fr aspell-it aspell-es aspell-de
+#
 
 TEXT=${1}
 DICT_1=${2-pt_BR}
-DICT_2=${3-en}
+DICT_2=${3-$DICT_1}
+DICT_3=${4-$DICT_2}
 
 [ -z "`which aspell`" ] && echo "Please install 'aspell'." >&2 && exit 1;
 
-function aspell_list {
-	local DICT=${1}
-	cat - | aspell -a -d "${DICT}" | grep "^&" | cut -d' ' -f2 | sort | uniq;
-}
+cat "${TEXT}" \
+	| aspell --master "${DICT_1}" list \
+	| aspell --master "${DICT_2}" list \
+	| aspell --master "${DICT_3}" list \
+	| sort | uniq -c | sort -n;
 
-cat "${TEXT}" | aspell_list "${DICT_1}" | aspell_list "${DICT_2}";
 
